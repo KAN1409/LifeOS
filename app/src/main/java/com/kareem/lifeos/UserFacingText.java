009f0e96a21339047eb490a72f8712eb08ab49c6
KAN1409 <engkareemnasser@gmail.com>
2026-09-04 23:33:40 +0300

    Canonicalize Kareem spelling and direct-address UI copy

--- app/src/main/java/com/kareem/lifeos/UserFacingText.java
+++ app/src/main/java/com/kareem/lifeos/UserFacingText.java
@@ -1,10 +1,12 @@
 package com.kareem.lifeos;
 
-/** Keeps internal model terminology out of LifeOS copy. */
+/** Keeps internal model terminology and third-person system copy out of LifeOS UI. */
 final class UserFacingText {
     private UserFacingText(){}
     static String humanize(String value){
         String x=value==null?"":value.trim();
+        x=x.replaceAll("(?i)\\bKarim's\\b","Kareem's");
+        x=x.replaceAll("(?i)\\bKarim\\b","Kareem");
         x=x.replaceAll("(?i)\\bthe user's\\b","your");
         x=x.replaceAll("(?i)\\bthe user is\\b","you are");
         x=x.replaceAll("(?i)\\bthe user has\\b","you have");
@@ -15,6 +17,8 @@ final class UserFacingText {
         x=x.replaceAll("(?i)\\bfor the user\\b","for you");
         x=x.replaceAll("(?i)\\bthe user\\b","you");
         x=x.replaceAll("(?i)\\buser's\\b","your");
+        x=x.replaceAll("(?i)(^|(?<=[.!?]\\s))User\\b","You");
+        x=x.replaceAll("(?i)\\bUser received\\b","You received");
         return x;
     }
 }
